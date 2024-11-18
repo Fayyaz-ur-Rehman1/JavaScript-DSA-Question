c38bea70d979b36eb221cb67dd106a1830a44432
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2024-11-19 02:10:47 +0530

    Filter-String-From-Array

--- Array-Question-Easy/js/Filter-String-from-Array.js
+++ Array-Question-Easy/js/Filter-String-from-Array.js
@@ -2,15 +2,18 @@
 
 function filterArray(arr) {
 
-    // return arr.filter(e => typeof e === "number")
-
-    let resultStore = [];
+    // forloop
+    let result = [];
     for (let i = 0; i < arr.length; i++) {
         if (typeof arr[i] === "number") {
-            resultStore.push(arr[i])
+            result.push(arr[i])
         }
     }
-    return resultStore
+    return result
+
+    // method
+    // return arr.filter(elm => typeof elm === "number");
+    // return arr.map(elm => typeof elm === "number" ? elm : )
 }
 
 console.log(filterArray([1, 2, 3, "a", "b", 4]));// ➞ [1, 2, 3, 4]
