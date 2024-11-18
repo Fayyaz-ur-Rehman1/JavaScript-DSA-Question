6e5efadf3719d9f31fb77df965de88a7b8703958
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2024-11-19 02:11:31 +0530

    True-Ones-False-Zero

--- Array-Question-Easy/js/True-Ones-Flase-Zero.js
+++ Array-Question-Easy/js/True-Ones-Flase-Zero.js
@@ -1,33 +1,39 @@
 // Create a function that returns an array of booleans from a given number by iterating through the number one digit at a time and appending true into the array if the digit is 1 and false otherwise.
 
 function integerBoolean(int) {
-    let spl = int.split("")
-    // return spl.map(function (e) {
-    //     if (e === "1") {
-    //         return true
-    //     }
-    //     return false
-    // })
 
-    return spl.reduce(function (accu, curr) {
-        if (curr === "1") {
-            accu.push(true)
+    // forloop
+    let spl = int.split("");
+    let result = [];
+    for (let i = 0; i < spl.length; i++) {
+        if (spl[i] === "1") {
+            result.push(true);
         } else {
-            accu.push(false)
+            result.push(false);
         }
-        return accu
+    }
+    return result;
 
-    }, [])
-
-    // let resultStore = []
     // for (let i = 0; i < int.length; i++) {
     //     if (int[i] === "1") {
-    //         resultStore.push(true)
+    //         result.push(true);
     //     } else {
-    //         resultStore.push(false)
+    //         result.push(false);
     //     }
     // }
-    // return resultStore
+    // return result
+
+    // method
+    // let spl = int.split("");
+    // return spl.map(elm => elm === "1");
+    // return spl.reduce(function (acc, curr) {
+    //     if (curr === "1") {
+    //         acc.push(true)
+    //     } else {
+    //         acc.push(false)
+    //     }
+    //     return acc
+    // }, [])
 }
 
 console.log(integerBoolean("100101")); // [true, false, false, true, false, true]
