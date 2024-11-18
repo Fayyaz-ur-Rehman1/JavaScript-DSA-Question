2835b928a5c3dd4c06f6971057dcbedcaba6b07a
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2024-11-19 02:04:22 +0530

    Remove-duplivated-from-sortlisted-Array

--- Array-Question-Easy/js/Remove-duplicates-from-shortlisted-Array.js
+++ Array-Question-Easy/js/Remove-duplicates-from-shortlisted-Array.js
@@ -1,17 +1,21 @@
 
-function RemoveShortlistedDup(arr){
-   let GetArr =  arr.sort((a,b) => a - b );
+function RemoveShortlistedDup(arr) {
+    let sortArray = arr.sort((a, b) => a - b);
 
-    let newArr = [];
-
-    for(let i = 0 ;  i < GetArr.length; i++){
-        if(GetArr[i] !== GetArr[i + 1]){
-            newArr.push(GetArr[i])
+    // forloop
+    let result = [];
+    for (let i = 0; i < sortArray.length; i++) {
+        if (sortArray[i] !== sortArray[i + 1]) {
+            result.push(sortArray[i]);
         }
     }
+    return result
 
-    return newArr
+    // method
+    // return sortArray.filter((itmes, index) => itmes !== sortArray[index + 1]);
+    // return [...new Set(sortArray)]
 }
+// console.log(RemoveShortlistedDup([5, 1, 3, 2, 4]));
 
-console.log(RemoveShortlistedDup([10,9,8,7,8,6,2,5,1,4,3,2,1]));
+console.log(RemoveShortlistedDup([10, 9, 8, 7, 8, 6, 2, 5, 1, 4, 3, 2, 1]));
 // console.log(RemoveShortlistedDup([1,2,3,4,5,5,6,7]));
