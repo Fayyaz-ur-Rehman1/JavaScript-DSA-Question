47bc5a033bef317d9df62115bae8223d589af1f4
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2024-11-21 01:20:29 +0530

    The-Forbidden-Letter

--- Array-Question-Easy/js/The-Forbidden-Letter.js
+++ Array-Question-Easy/js/The-Forbidden-Letter.js
@@ -1,22 +1,15 @@
 // Given a letter and an array of words, return whether the letter does not appear in any of the words.
 
 function forbiddenLetter(words, array) {
-    // for (let i = 0; i < array.length; i++) {
-    //     if (array[i].includes(words)) {
-    //         return false
-    //     }
-    // }
-    // return true
-
-    let forBidden = true
-    array.forEach(elm => {
-        if (elm.includes(words)) {
-            forBidden = false
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].includes(words)) {
+            return false
         }
-    })
-    return forBidden
+    }
+    return true
 
-    // return array.every(elm => !elm.includes(words))
+    // return array.every(elm => !elm.includes(words));
+    // return !array.some(elm => elm.includes(words));
 }
 
 console.log(forbiddenLetter("r", ["rock", "paper", "scissors"])); // false
