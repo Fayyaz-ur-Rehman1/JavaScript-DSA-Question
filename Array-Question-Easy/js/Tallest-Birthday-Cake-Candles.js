d09cda37cf8dd025e0efe0b2f8cc193f9370b6c3
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2024-11-22 00:19:53 +0530

    Tallest-Birthday-Cake-Candle

--- Array-Question-Easy/js/Tallest-Birthday-Cake-Candles.js
+++ Array-Question-Easy/js/Tallest-Birthday-Cake-Candles.js
@@ -1,22 +1,33 @@
 // console.log(You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
 
 function birthdayCakeCandles(array) {
-
-    let max = Math.max(...array);
-    return array.filter(elm => elm === max).length
-
-    // let maximum = -Infinity;
+    // let max = -Infinity;
     // let count = 0;
-
     // for (let i = 0; i < array.length; i++) {
-    //     if (maximum < array[i]) {
-    //         maximum = array[i]
-    //         count++
-    //     } else if (maximum === array[i]) {
+    //     if (array[i] > max) {
+    //         max = array[i];
+    //         count = 1;
+    //     } else if (array[i] === max) {
+    //         count++;
+    //     }
+    // }
+
+    // return count;
+
+
+    // method
+    // let count = 0;
+    // let max = Math.max(...array);
+    // for (let i = 0; i < array.length; i++) {
+    //     if (array[i] === max) {
     //         count++
     //     }
     // }
     // return count
+
+    let count = 0;
+    let max = Math.max(...array);
+    return array.filter(elm => elm === max).length
 }
 
 console.log(birthdayCakeCandles([4, 4, 1, 3])); // 2
