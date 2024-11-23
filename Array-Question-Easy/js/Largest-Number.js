01d9b5a65da7818af83ae4856ad31b1223dd866f
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2024-11-23 13:25:27 +0530

    Find-Largest-Number

--- Array-Question-Easy/js/Largest-Number.js
+++ Array-Question-Easy/js/Largest-Number.js
@@ -2,23 +2,26 @@
 
 
 function largestNumbers(n, array) {
-    let resultStore = [];
+    // loops
+    if (n <= 0) return [];
+    let result = [];
 
     for (let i = 0; i < n; i++) {
         let max = -Infinity;
-        let maxIndex = -1;
+
         for (let j = 0; j < array.length; j++) {
-            if (max < array[j]) {
-                max = array[j]
-                maxIndex = j
+            if (!result.includes(array[j]) && array[j] > max) {
+                max = array[j];
             }
         }
-        if (maxIndex !== -1) {
-            resultStore.push(max)
-            array[maxIndex] = -Infinity
-        }
+        result.push(max);
     }
-    return resultStore
+
+    return result;
+
+    // method
+    // let sortedArray = [...array].sort((a, b) => b - a);
+    // return sortedArray.slice(0, n);
 }
 
 // Example usage:
@@ -26,5 +29,3 @@ console.log(largestNumbers(2, [4, 3, 2, 1])); // Output: [4, 3]
 console.log(largestNumbers(1, [7, 19, 4, 2])); // Output: [19]
 console.log(largestNumbers(3, [14, 12, 57, 11, 18, 16])); // Output: [57, 18, 16]
 console.log(largestNumbers(0, [1, 3, 4, 2])); // Output: []
-
-
