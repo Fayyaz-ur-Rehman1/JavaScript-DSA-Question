2b2dbb22546fd02b73d57747b62d109991ab9e25
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2024-11-24 18:42:08 +0530

    return-Last-item

--- Array-Question-Easy/js/retrun-Last-Item.js
+++ Array-Question-Easy/js/retrun-Last-Item.js
@@ -1,11 +1,29 @@
 // Create a function that returns the last value of the last item in an array or string.
 
 function lastItem(array) {
-    let lastItem = "";
     for (let i = array.length - 1; i < array.length; i++) {
-        lastItem += array[i];
+        return array[i]
     }
-    return lastItem
+
+    // method
+    // if (Array.isArray(array)) {
+    //     return array[array.length - 1];
+    // } else if (typeof array === "string") {
+    //     return array[array.length - 1];
+    // } else {
+    //     return undefined;
+    // }
+
+    // if (Array.isArray(array) || typeof array === "string") {
+    //     return array.slice(-1)[0];
+    // } else {
+    //     return undefined;
+    // }
+
+    //     if (array.length === 0) {
+    //         return undefined;
+    //     }
+    //     return array[array.length - 1];
 }
 
 console.log(lastItem([0, 4, 19, 34, 50, -9, 2])); // 2
