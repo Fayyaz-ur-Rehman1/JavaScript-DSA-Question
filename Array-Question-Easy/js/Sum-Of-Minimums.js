35dd5a5c00b72a7811a5445098b3ad7f63e6cb29
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2024-11-25 14:59:49 +0530

    Sum-Of-Minimums-Number

--- Array-Question-Easy/js/Sum-Of-Minimums.js
+++ Array-Question-Easy/js/Sum-Of-Minimums.js
@@ -1,18 +1,24 @@
 // Create a function that takes a 2D array arr and returns the sum of the minimum value in each row.
 
 function sumMinimums(arr) {
-    let sum = 0;
+    // // loops
+    // let total = 0;
+    // for (let i = 0; i < arr.length; i++) {
+    //     let min = +Infinity;
+    //     for (let j = 0; j < arr[i].length; j++) {
+    //         if (arr[i][j] < min) {
+    //             min = arr[i][j]
+    //         }
+    //     }
+    //     total += min;
+    // }
+    // return total;
 
-    for (let i = 0; i < arr.length; i++) {
-        let min = +Infinity;
-        for (let j = 0; j < arr[i].length; j++) {
-            if (arr[i][j] < min) {
-                min = arr[i][j]
-            }
-        }
-        sum += min;
-    }
-    return sum
+    // method
+    // let total = arr.map(elm => Math.min(...elm)).reduce((a, b) => a + b, 0);
+    // return total;
+
+    return arr.flatMap(subArray => [Math.min(...subArray)]).reduce((sum, min) => sum + min, 0);
 }
 
 console.log(sumMinimums([
@@ -21,6 +27,11 @@ console.log(sumMinimums([
     [20, 21, 34, 56, 100]
 ]));  // 26
 
+console.log(sumMinimums([
+    [1, 2, 3, 4, 5],
+    [5, 6, 7, 8, 9]
+]));  // 6
+
 // minimum value of the first row is 1
 // minimum value of the second row is 5
-// minimum value of the third row is 20111
+// minimum value of the third row is 20
