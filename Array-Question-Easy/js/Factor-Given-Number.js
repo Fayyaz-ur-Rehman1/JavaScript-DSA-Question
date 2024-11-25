ee00892224818c45025f8363aa33100b176313f2
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2024-11-25 14:31:22 +0530

    Factor-Given-Number

--- Array-Question-Easy/js/Factor-Given-Number.js
+++ Array-Question-Easy/js/Factor-Given-Number.js
@@ -1,14 +1,19 @@
 // Create a function that finds each factor of a given number n.Your solution should return an array of the number(s) that meet this criteria.
 
 
-function findFactors(num) {
-    let resultStore = [];
-    for (let i = 1; i < num; i++) {
-        if (num % i === 0) {
-            resultStore.push(i)
-        }
-    }
-    return resultStore
+function findFactors(n) {
+    // if (n === 0) return [];
+    // let factor = [];
+    // for (let i = 0; i <= n; i++) {
+    //     if (n % i === 0) {
+    //         factor.push(i);
+    //     }
+    // }
+    // return factor;
+
+    if (n === 0) return [];
+    return Array.from({ length: n }, (_, i) => i + 1)
+        .filter(i => n % i == 0);
 }
 
 console.log(findFactors(9));  // [1, 3, 9]
