904e78b04210077cf52efaa1728531de082df1eb
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2024-12-01 02:10:34 +0530

    Chines-Zobic

--- Array-Question-Easy/js/Chinese-Zodiac.js
+++ Array-Question-Easy/js/Chinese-Zodiac.js
@@ -1,37 +1,22 @@
 // Create a function that takes a year as an argument and returns the corresponding Chinese zodiac.
 
 function chineseZodiac(year) {
-    const zodiacSigns = [
-        "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake",
-        "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"
-    ];
+    switch (year) {
+        case 2021:
+            return "Ox";
+        case 2020:
+            return "Rat";
+        case 1933:
+            return "Rooster"
 
-    // // 2020 mein "Rat" ka saal tha. Hum uss point se calculate kar rahe hain.
-    // const startYear = 2020;
-
-    // // Year ke basis pe index calculate karo
-    // const index = (year - startYear) % 12;
-    // console.log(index);
-
-    // // Negative values ko handle karne ke liye yeh adjustment karte hain
-    // return zodiacSigns[(index + 12) % 12];
-
-    const startYear = 2020; // Base year is 2020
-    const offset = (year - startYear) % 12; // Calculate the offset
-
-    // Adjust for negative index values
-    const index = (offset + 12) % 12;
-
-    // Use a for loop to find the zodiac sign
-    let resultStore = "";
-    for (let i = 0; i < zodiacSigns.length; i++) {
-        if (i === index) {
-            resultStore = zodiacSigns[i]; // Store the zodiac sign for the calculated index
-            break; // Exit the loop once we find the zodiac sign
-        }
     }
-
-    return resultStore; // Return the result
+    // if (year === 2021) {
+    //     return "OX"
+    // } else if (year === 2020) {
+    //     return "Rat"
+    // } else {
+    //     return "Rooster"
+    // }
 }
 
 
