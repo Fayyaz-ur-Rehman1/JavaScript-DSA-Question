0d1c82e0f72c5b005c712d55363dc9886ed27515
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2025-01-23 13:48:48 +0530

    Maths-Question

---
+++ Maths-Question/js/Performs-Basic-Arthametic-Opreators.js
@@ -0,0 +1,23 @@
+// Perform basic Arthametic Opreators  take a and b perform Addtion Multiplication Subraction Division and Modules
+
+function Addtion(a, b) {
+    return a + b;
+}
+function Multiplication(a, b) {
+    return a * b;
+
+}
+function Subraction(a, b) {
+    return a - b;
+}
+function Division(a, b) {
+    return a / b;
+}
+function Modules(a, b) {
+    return a % b
+}
+console.log(Addtion(3, 7)) // 10
+console.log(Subraction(5, 4)) // 1
+console.log(Multiplication(10, 45)) // 450
+console.log(Division(10, 10)) // 0.6615...
+console.log(Modules(10, 2)) // 0
