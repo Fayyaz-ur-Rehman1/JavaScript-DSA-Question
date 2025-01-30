83299709471b8698f2bc3119df361178cb7386a9
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2025-01-31 00:20:25 +0530

    Return-the-Sum-of-Two-Number

---
+++ Maths-Question/js/Return-the-Sum-of-Two-Number.js
@@ -0,0 +1,13 @@
+// Create a function that takes two numbers as arguments and returns their sum.
+
+function addition(num1, num2) {
+    // return num1 + num2
+    // return num1 / 1 + num2 / 1
+    // return num1 - 1 + 1 + num2
+    // return num1 * 1 + num2 * 1
+    return +num1 + +num2;
+}
+
+console.log(addition(3, 2)); // 5
+console.log(addition(-3, -6)); // -9
+console.log(addition(7, 3)); // 10
