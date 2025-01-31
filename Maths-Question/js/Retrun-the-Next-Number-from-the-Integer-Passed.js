7c2302c91199bb7087d35260821f43262ed359db
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2025-01-31 17:51:04 +0530

    Return-the-Sum-of-Two-Number

---
+++ Maths-Question/js/Retrun-the-Next-Number-from-the-Integer-Passed.js
@@ -0,0 +1,11 @@
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+function addition(increments) {
+    // return ++increments
+    // return 1 + increments
+    return increments + 1
+}
+
+console.log(addition(0)); // 1
+console.log(addition(9)); // 10
+console.log(addition(-3)); // -2
