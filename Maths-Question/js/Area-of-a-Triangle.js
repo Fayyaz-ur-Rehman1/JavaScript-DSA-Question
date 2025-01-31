ecefedff6b38c533333373f1536b984a3e19a46c
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2025-01-31 17:46:58 +0530

    Area-of-a-Triangle

---
+++ Maths-Question/js/Area-of-a-Triangle.js
@@ -0,0 +1,9 @@
+// Write a function that takes the base and height of a triangle and return its area.
+
+function triArea(base, height) {
+    return (base * height) / 2;
+}
+
+console.log(triArea(3, 2)); // 3
+console.log(triArea(7, 4)); // 14
+console.log(triArea(10, 10)); // 50
